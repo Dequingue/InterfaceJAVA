9a16f6c1069194f6de11ae46e716527874023868
Dequingue <159256601+Dequingue@users.noreply.github.com>
2025-05-08 16:01:37 -0300

    Add files via upload

---
+++ GerenciamentoEstoque/src/main/java/com/mycompany/gerenciamentoestoque/GerenciamentoEstoque.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gerenciamentoestoque;
+import View.*;
+/**
+ *
+ * @author aluno.den
+ */
+public class GerenciamentoEstoque {
+
+    public static void main(String[] args) {
+        TelaLogin lg = new TelaLogin();
+        lg.setVisible(true);
+        
+    }
+}
